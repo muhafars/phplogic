2ea0e0594ad2e705d737bdb283733093eb07d18d
Muhammad Fajri Assidiq <muhafash@users.noreply.github.com>
2021-05-05 05:45:43 +0700

    memasukkan file

--- muhammadfajriassidiq_soalno_1.php
+++ muhammadfajriassidiq_soalno_1.php
@@ -2,7 +2,19 @@
 function kalkulatorSederhana($harga, $jumlah, $pembayaran){
     $hasil= $harga * $jumlah;
     $kembalian = $pembayaran - $hasil;
-    echo ('kembalian' + $kembalian);
+   return $kembalian;
 }
-kalkulatorSederhana(1000, 6, 7000);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Uang kembalian yang diterima andi  Rp.<?= kalkulatorSederhana(1000, 6, 7000);
+?></p>
+</body>
+</html>
